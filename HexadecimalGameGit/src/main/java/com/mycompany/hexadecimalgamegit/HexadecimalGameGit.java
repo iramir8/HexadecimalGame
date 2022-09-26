0fac470a65bc5cb72728727549b47fe4ed3f3141
Isaiah <>
2022-09-25 21:05:39 -0500

    Hello Team

---
+++ HexadecimalGameGit/src/main/java/com/mycompany/hexadecimalgamegit/HexadecimalGameGit.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.hexadecimalgamegit;
+
+public class HexadecimalGameGit {
+
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World!");
+    }
+}
